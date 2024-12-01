804748dd2cd861b906107dd108de3611d112abf0
Noah Iniguez <niniguezg@gmail.com>
2024-12-01 09:20:32 -0600

    Implement day 1 part 2.

--- 01/main.py
+++ 01/main.py
@@ -1,27 +1,57 @@
+"""Day 1: Historian Hysteria."""
+
 import argparse
 
 
 def main():
+    """
+    Determine the similarity score of two lists.
+
+    Calculate a total similarity score by adding up each number in the left list
+    after multiplying it by the number of times that number appears in the right list.
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("filename")
     args = parser.parse_args()
+
     with open(args.filename, mode="r", encoding="utf-8") as f:
-        parse(f)
+        first, second = parse(f)
+
+    second.sort()
+    total_similarity = 0
+    for i in first:
+        total_similarity += i * similarity(i, second)
+
+    print(total_similarity)
 
 
 def parse(f):
+    """Parse a file and return two integer lists."""
     first = []
     second = []
     for line in f:
         split = line.split()
-        first.append(split[0])
-        second.append(split[1])
-    first.sort()
-    second.sort()
-    total = 0
-    for i, k in enumerate(first):
-        total += abs(int(k, 10) - int(second[i], 10))
-    print(total)
+        first.append(int(split[0], 10))
+        second.append(int(split[1], 10))
+    return first, second
+
+
+def similarity(num, arr):
+    """
+    Find the number of times `num` appears in `arr`.
+
+    Expects `arr` to be sorted.
+    """
+    count = 0
+    for i in arr:
+        if num > i:
+            continue
+        if num == i:
+            count += 1
+            continue
+        if num < i:
+            break
+    return count
 
 
 if __name__ == "__main__":
